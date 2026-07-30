42bf31f93e6e4edfd73011bf9e77b5c9d00281da
warmthunder <hriday.singh99@gmail.com>
2026-07-31 00:16:32 +0530

    uploaded basic logo and updates instructions

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <sys/types.h>
 
 #define stack_length 16
@@ -43,6 +44,7 @@ typedef struct{
     uint8_t sound_timer;
     // bool keypress[16];
     uint32_t display[64*32];
+    uint16_t I;
 }chip8;
 
 // SDL
@@ -50,22 +52,53 @@ SDL_Window *window;
 SDL_Renderer *renderer;
 bool quit = false;
 int movement[4] = {0,0,0,0};
+bool keys[16];
 
 static void sdl_process_event(SDL_Event *event){
    switch(event->type){
+    SDL_Keycode key = event->key.keysym.sym;
         case SDL_KEYDOWN:
-            SDL_Keycode key = event->key.keysym.sym;
-            if(key==SDLK_s)
-                movement[1] = +10;
-            if(key==SDLK_w)
-                movement[1] = -10;
+            switch(key){
+                case SDLK_s:
+                    keys[0] = true;
+                    break;
+                case SDLK_w:
+                    keys[1] = true;
+                    break;
+                case SDLK_a:
+                    keys[2] = true;
+                    break;
+                case SDLK_d:
+                    keys[3] = true;
+                    break;
             break;
+            }
+            // if(key==SDLK_s)
+            //     movement[1] = +10;
+            // if(key==SDLK_w)
+            //     movement[1] = -10;
+            // break;
          case SDL_KEYUP:
-            key = event->key.keysym.sym;
-            if(key==SDLK_s)
-                movement[1] = 0;
-            if(key==SDLK_w)
-                movement[1]  = 0;
+               switch(key){
+                case SDLK_s:
+                    keys[0] = false;
+                    break;
+                case SDLK_w:
+                    keys[1] = false;
+                    break;
+                case SDLK_a:
+                    keys[2] = false;
+                    break;
+                case SDLK_d:
+                    keys[3] = false;
+                    break;
+            break;
+            }
+
+            // if(key==SDLK_s)
+            //     movement[1] = 0;
+            // if(key==SDLK_w)
+            //     movement[1]  = 0;
             break;
         case SDL_QUIT:{
             quit = true;
@@ -89,6 +122,8 @@ uint8_t x = (opcode & 0x0F00) >> 8;
 uint8_t y = (opcode & 0x00F0) >> 4;
 uint16_t sum = instance->registers[x] + instance->registers[y];
 uint8_t vF = 0xF;
+uint8_t n3 = opcode & 0x0FFF;
+uint8_t n2 = opcode & 0x00FF;
 
 switch(opcode){
     case 0x00E0:
@@ -128,66 +163,135 @@ switch(opcode & 0xF000){
         instance->registers[x] += opcode & 0x00FF;
         break;
     case 0x8000:
-        instance->registers[x] = instance->registers[y];
-        break;
         switch(opcode & 0x000F){
-        case 0x8001:
-            instance->registers[x] = instance->registers[x] | instance->registers[y];
-            break;
-        case 0x8002:
-            instance->registers[x] = instance->registers[x] & instance->registers[y];
-            break;
-        case 0x8003:
-            instance->registers[x] = instance->registers[x] ^ instance->registers[y];
-            break;
-        case 0x8004:
-            if(sum>0xFF){
-                instance->registers[x] = sum & 0xFF;
-                instance->registers[vF] = 1;
-            }
-            else {
-                instance->registers[x] =sum;
-                instance->registers[vF] = 0;
-            }
-        case 0x8005:
-            if(instance->registers[x]>=instance->registers[y]){
-                instance->registers[x] = instance->registers[x] - instance->registers[y];
-                instance->registers[vF] = 0;
-            }
-            else{
-                instance->registers[vF] = 1;
-            }
-            break;
-        case 0x8006:
-            if(instance->registers[x]%2==0){
-                instance->registers[x] = instance->registers[x]>>1;
-                instance->registers[vF] = 0;
-            }
-            else{
-                instance->registers[x] = instance->registers[x]>>1;
-                instance->registers[vF] = 1;
-            }
-            break;
-        case 0x8007:
-                if(instance->registers[y]>instance->registers[x]){
+            case 0x0:
+                instance->registers[x] = instance->registers[y];
+                break;
+            case 0x1:
+                instance->registers[x] = instance->registers[x] | instance->registers[y];
+                break;
+            case 0x2:
+                instance->registers[x] = instance->registers[x] & instance->registers[y];
+                break;
+            case 0x3:
+                instance->registers[x] = instance->registers[x] ^ instance->registers[y];
+                break;
+            case 0x4:
+                if(sum>0xFF){
+                    instance->registers[x] = sum & 0xFF;
                     instance->registers[vF] = 1;
                 }
+                else {
+                    instance->registers[x] =sum;
+                    instance->registers[vF] = 0;
+                }
+            case 0x5:
+                if(instance->registers[x]>=instance->registers[y]){
+                    instance->registers[x] = instance->registers[x] - instance->registers[y];
+                    instance->registers[vF] = 0;
+                }
                 else{
-                    instance->registers[x]-=instance->registers[y];
+                    instance->registers[vF] = 1;
+                }
+                break;
+            case 0x6:
+                if(instance->registers[x]%2==0){
+                    instance->registers[x] = instance->registers[x]>>1;
+                    instance->registers[vF] = 0;
+                }
+                else{
+                    instance->registers[x] = instance->registers[x]>>1;
+                    instance->registers[vF] = 1;
+                }
+                break;
+            case 0x7:
+                    if(instance->registers[y]>instance->registers[x]){
+                        instance->registers[vF] = 1;
+                    }
+                    else{
+                        instance->registers[x]-=instance->registers[y];
+                    }
+                break;
+            case 0xE:
+                if(instance->registers[x]%2==0){
+                    instance->registers[x] = instance->registers[x]<<1;
+                    instance->registers[vF] = 0;
+                }
+                else{
+                    instance->registers[x] = instance->registers[x]<<1;
+                    instance->registers[vF] = 1;
                 }
             break;
-        case 0x800E:
-            if(instance->registers[x]%2==0){
-                instance->registers[x] = instance->registers[x]<<1;
-                instance->registers[vF] = 0;
-            }
-            else{
-                instance->registers[x] = instance->registers[x]<<1;
-                instance->registers[vF] = 1;
-            }
-            break;
+        break;
     }
+    case 0x9000:
+        if(instance->registers[x]!=instance->registers[y]){
+            instance->program_counter += 2;
+        }
     break;  
+    case 0xA000:
+        instance->I = n3;
+    break;
+    case 0xB000:
+        instance->program_counter = n3 + instance->registers[0];
+    break;
+    case 0xC000:
+        instance->registers[x] = (rand()%256) & n2;
+    break;
+    case 0xD000:
+
+    break;
+    
+    case 0xE000:
+        switch(opcode & 0xFF){
+            case 0x9E:
+                if(movement[x])
+                    instance->program_counter+=2;
+            break;
+            case 0xA1:
+                if(!movement[x])
+                    instance->program_counter+=2;
+            break;
+        break;
+        }
+        
+    case 0xF000:
+        switch(opcode & 0xFF){
+            case 0x07:
+                instance->registers[x] = instance->delay_timer;
+            break;
+            case 0x0A:
+                // wait for a keypress
+            break;
+            case 0x15:
+                instance->delay_timer = instance->registers[x];
+            break;
+            case 0x18:
+                instance->sound_timer = instance->registers[x];
+            break;
+            case 0x1E:
+                instance->I += instance->registers[x];
+            break;
+            case 0x29:
+                // set I to the location for the hexadeciaml spride of value vx
+            break;
+            case 0x33:
+                // what the fuck
+            break;
+            case 0x55:
+                // what
+                // for(int i = instance->I; i<=instance->I+)
+            break;
+            case 0x65:
+                // what
+            break;
+        break;
+        }
+    break;
+        
+
+break;
+
 }
 
 }
